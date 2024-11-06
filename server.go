76fdcd46447fab86b44e36befd5e3c9c205a9de0
mateusdeitos <mate.deitos@hotmail.com>
2024-11-06 15:52:31 -0300

    injetando configmap em um volume

--- server.go
+++ server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", hello)
 
 	http.ListenAndServe(":8000", nil)
@@ -16,4 +18,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	w.Write([]byte(fmt.Sprintf("Hello, %s. I am %s years old.", name, age)))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+		return
+	}
+
+	fmt.Fprint(w, string(data))
 }

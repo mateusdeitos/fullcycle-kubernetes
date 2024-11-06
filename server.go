8a58b46db7a9afdbf46a6e830342e367e05ccec9
mateusdeitos <mate.deitos@hotmail.com>
2024-11-06 16:30:44 -0300

    secret

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", hello)
 
@@ -18,6 +19,12 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	w.Write([]byte(fmt.Sprintf("Hello, %s. I am %s years old.", name, age)))
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASSWORD")
+	w.Write([]byte(fmt.Sprintf("User: %s, Password: %s", user, pass)))
 }
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
